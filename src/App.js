6c100eced09773bd5d7886e53ca220ee3bbb7ec7
Santiago Martinez Campari <santidreik@gmail.com>
2021-11-03 23:12:35 -0300

    first commit
    a

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import CartWidget from './components/CartWidget/CartWidget';
+import ItemCount from './components/ItemCount/ItemCount';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/ItemCount/ItemCount.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <NavBar>
+        <CartWidget/>
+      </NavBar>
+      <ItemListContainer greeting="Hola, comprador"/>
+      <ItemCount stock={5} initial={1}/>
+    </div>
+  );
+}
+
+export default App;
